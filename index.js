e0cacb7c6f565834e0d7042b0586d749648fa226
Syed Ahmad <ahmadtaqi789@gmail.com>
2018-01-12 17:56:38 +0530

    added socket.io to control connections

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 var app = require('express')();
 var http = require('http').Server(app);
-
+var io = require('socket.io')(http);
+//rsponds to get request on port 300 by sending the index.html file as result
 app.get('/', function(req, res){
-  res.send('<h1>Hello world</h1>');
+  res.sendFile(__dirname + '/index.html');
 });
-
+//controls connection by different users on port 3000
+io.on('connection', function(socket){
+  console.log('a user connected');
+});
+//listening on port 3000
 http.listen(3000, function(){
   console.log('listening on *:3000');
 });
