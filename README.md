426a517ee8d54d8c9621c8e8d36a97482f96061c
Syed Ahmad <ahmadtaqi789@gmail.com>
2018-01-12 22:49:56 +0530

    hosted static files to page

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Chat-app
+
+A simple real time chat application which can be used by large number of users at once.
+
+Tools used: HTML, CSS, Node.js, Express, Socket.io
--- index.js
+++ index.js
@@ -1,22 +1,34 @@
-var app = require('express')();
+var express = require('express');
+var app=express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+
+//to host static files to the html ie stylesheets and javascript
+var path = require('path');
+app.use(express.static(path.join(__dirname,'public')));
 
 //responds to get request on port 300 by sending the index.html file as result
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
-//controls connection by different users on port 3000
+//io socket which on receiving chat message event on server emits chat message event to clients with the message data
 io.on('connection', function(socket){
-	console.log('a user connected');
-	//function when user disconnects ie closes the window or tab
-	socket.on('disconnect', function(){
-		console.log('user disconnected');
+	 socket.on('chat message', function(msg){
+    io.emit('chat message', msg);
+  });
+	
 });
 
 //listening on port 3000
 http.listen(3000, function(){
   console.log('listening on *:3000');
 });
+
+/*console.log('a user connected');
+	socket.on('chat message', function(msg){
+    console.log('message: ' + msg);
+	//function when user disconnects ie closes the window or tab
+	socket.on('disconnect', function(){
+		console.log('user disconnected');*/
     
